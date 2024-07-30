b202aeffea79b596e8de8506bc8abc2d1e14936e
Derek Hayes <derek2099@gmail.com>
2024-07-30 08:24:30 -0500

    Remove help button

--- app/src/main/java/dev/derekhayes/vacationplanner/ui/MainActivity.java
+++ app/src/main/java/dev/derekhayes/vacationplanner/ui/MainActivity.java
@@ -2,8 +2,6 @@ package dev.derekhayes.vacationplanner.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
