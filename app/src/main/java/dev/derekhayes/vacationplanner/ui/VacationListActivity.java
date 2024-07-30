2085c78f7f9b17163b7c23bd317eb54653185967
Derek Hayes <derek2099@gmail.com>
2024-07-30 07:28:14 -0500

    Add FAB to add a vacation

---
+++ app/src/main/java/dev/derekhayes/vacationplanner/ui/VacationListActivity.java
@@ -0,0 +1,33 @@
+package dev.derekhayes.vacationplanner.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import dev.derekhayes.vacationplanner.R;
+
+public class VacationListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_vacation_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        findViewById(R.id.add_vacation_button).setOnClickListener(view -> addVacation());
+    }
+
+    private void addVacation() {
+        startActivity(new Intent(this, VacationDetailActivity.class));
+    }
+}
