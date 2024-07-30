05d7e88c183339c5b96e216deb632c1f1d7a7bc7
Derek Hayes <derek2099@gmail.com>
2024-07-30 07:27:09 -0500

    Edit layout of homepage. Add link to VacationListActivity

--- app/src/main/java/dev/derekhayes/vacationplanner/ui/MainActivity.java
+++ app/src/main/java/dev/derekhayes/vacationplanner/ui/MainActivity.java
@@ -1,12 +1,17 @@
-package dev.derekhayes.vacationplanner;
+package dev.derekhayes.vacationplanner.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import dev.derekhayes.vacationplanner.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +25,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        findViewById(R.id.start_button).setOnClickListener(view -> onClickGetStarted());
+    }
+
+    public void onClickGetStarted() {
+        Intent intent = new Intent(this, VacationListActivity.class);
+        startActivity(intent);
     }
 }
