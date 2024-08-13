f3d1742515363850c0a88c59ff61988f97636971
Derek Hayes <derek2099@gmail.com>
2024-08-12 21:08:31 -0500

    Create menu option to create sample data

--- app/src/main/java/dev/derekhayes/vacationplanner/ui/VacationListActivity.java
+++ app/src/main/java/dev/derekhayes/vacationplanner/ui/VacationListActivity.java
@@ -24,6 +24,8 @@ import dev.derekhayes.vacationplanner.ui.adapter.VacationAdapter;
 
 public class VacationListActivity extends AppCompatActivity {
 
+    VacationRepository repo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,7 +64,7 @@ public class VacationListActivity extends AppCompatActivity {
 
     private void setRecyclerView() {
         RecyclerView recyclerView = findViewById(R.id.vacation_recycler);
-        VacationRepository repo = new VacationRepository(getApplication());
+        repo  = new VacationRepository(getApplication());
         List<Vacation> vacations;
         try {
             vacations = repo.getVacations();
@@ -90,11 +92,45 @@ public class VacationListActivity extends AppCompatActivity {
             onSearchRequested();
             return true;
         }
-        if (item.getItemId() == R.id.report) {
+        else if (item.getItemId() == R.id.report) {
             Intent intent = new Intent(this, ReportActivity.class);
             startActivity(intent);
         }
+        else if (item.getItemId() == R.id.sample_data) {
+            try {
+                createSampleDate();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
         return false;
+    }
+
+    private void createSampleDate() throws InterruptedException {
+        repo = new VacationRepository(getApplication());
+
+        Vacation vacation = new Vacation("Las Vegas", "Hilton", "9/10/2024", "9/17/2024", "Gambling fun times");
+        repo.addVacation(vacation);
+        vacation = new Vacation("Disney Land", "Magic Castle", "9/19/2024", "9/22/2024", "Family fun times");
+        repo.addVacation(vacation);
+        vacation = new Vacation("Grand Canyon", "Colorado Castle", "10/10/2024", "10/27/2024", "Big hole fun times");
+        repo.addVacation(vacation);
+        vacation = new Vacation("Paris", "Louvre", "11/05/2024", "11/16/2024", "Weird food fun times");
+        repo.addVacation(vacation);
+        vacation = new Vacation("Bora Bora", "Treehouse", "12/19/2024", "12/22/2024", "Volcanic fun times");
+        repo.addVacation(vacation);
+        vacation = new Vacation("Glacier Park", "Ice Berg Motel", "1/1/2025", "1/9/2025", "Cold fun times");
+        repo.addVacation(vacation);
+        vacation = new Vacation("Rome", "St. Peter's Basilica", "2/22/2025", "2/28/2025", "Ancient fun times");
+        repo.addVacation(vacation);
+        vacation = new Vacation("Swiss Alps", "St Moritz", "3/5/2025", "3/18/2025", "Skiing fun times");
+        repo.addVacation(vacation);
+        vacation = new Vacation("Maui", "Haleakala Hotel", "4/8/2025", "4/11/2025", "Luau fun times");
+        repo.addVacation(vacation);
+        vacation = new Vacation("London", "Tower of London", "5/1/2025", "11/2/2048", "Prison fun times");
+        repo.addVacation(vacation);
+
+        setRecyclerView();
     }
 }
